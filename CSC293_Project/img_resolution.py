8b2a49d2c47342d360e1f8690ac18fd11d1f4fd9
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2024-12-24 14:35:34 +0100

    Remove unused files and clean up project structure

--- CSC293_Project/img_resolution.py
+++
@@ -1,6 +0,0 @@
-import pytube
-
-url = "https://www.youtube.com/watch?v=Dvzg5Nl2gF0"
-path = r"C:\Users\owner\Desktop\React\CSC293_Project\Videos"
-pytube.YouTube(url).streams.get_highest_resolution().download(path)
-print("Downloaded")
