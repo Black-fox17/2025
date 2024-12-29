5d821643fab1882278bc57af7fe390ed192be818
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2024-12-29 13:44:35 +0100

    Add initial project structure with configuration files, controllers, and basic routing

--- SalamPick/bash.sh
+++
@@ -1,8 +0,0 @@
-npx create-instantsearch-app@latest instantsearch-app `
-    --template 'InstantSearch.js' `
-    --app-id '5TE814F15R' `
-    --api-key 'f41fad4c8a7ee10a31df32a24860b97d' `
-    --index-name 'products' `
-    --attributes-to-display 'products.0.name,products.0.description,products.0.price' `
-    --no-interactive `
-    --image-attribute 'products.0.image_url'
