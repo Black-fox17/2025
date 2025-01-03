cbea0a97617db68afd5e4db6fc2cd38ebff86e38
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2025-01-03 02:44:55 +0100

    Add Hero component to App; include introductory text and styling

--- Portfolio/main/src/App.jsx
+++ Portfolio/main/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './sections/Navbar.jsx';
+import Hero from './sections/Hero.jsx';
 
 function App (){
   return(
     <main className="max-w-7xl mx-auto text-center">
       <Navbar />
+      <Hero />
     </main>
   )
 }
