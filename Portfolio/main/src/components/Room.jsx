24814db5294908fc0a760705e57e0fdaa8953b79
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2025-01-03 05:04:32 +0100

    Enhance Room component with new textures and integrate Leva for controls; update Hero section for improved scaling and positioning

--- Portfolio/main/src/components/Room.jsx
+++ Portfolio/main/src/components/Room.jsx
@@ -3,6 +3,9 @@ import { useGLTF, useTexture } from '@react-three/drei';
 function Room(props) {
   const { nodes, materials } = useGLTF('/models/hacker-room.glb');
 
+  const monitortxt = useTexture('textures/desk/monitor.png');
+  const screenTxt = useTexture('textures/desk/screen.png');
+
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.screen_screens_0.geometry} material={materials.screens}>
--- Portfolio/main/src/sections/Hero.jsx
+++ Portfolio/main/src/sections/Hero.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Suspense } from 'react';
 import {Canvas} from '@react-three/fiber';
+import { Leva, useControls} from 'leva';
 import { PerspectiveCamera } from '@react-three/drei';
 import Room from '../components/Room';
 import CanvasLoader from '../components/CanvasLoader';
+import { useMediaQuery } from 'react-responsive';
 
 
 const Hero = () => {
+    const x = useControls('Room', 
+      {scalex:{value:0.07, min:0.01, max:0.1}, 
+      scaley:{value:0.07, min:0.01, max:0.1},
+      scalez:{value:0.07, min:0.01, max:0.1},
+      positionx:{value:0, min:-10, max:10},
+      positiony:{value:0, min:-10, max:10},
+      positionz:{value:0, min:-10, max:10},
+      rotationx:{value:0, min:0, max:360},
+      rotationy:{value:280, min:0, max:360},
+      rotationz:{value:0, min:0, max:360},
+
+      }
+    );
+    const ismobile = useMediaQuery({maxWidth: 768});
     return (
         <section className="min-h-screen w-full flex flex-col relative" id="home">
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -16,10 +32,12 @@ const Hero = () => {
                 <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
             </div>
             <div className="w-full h-full absolute inset-0">
+            <Leva />
                 <Canvas className="w-full h-full">
                   <Suspense fallback={<CanvasLoader />}>
+                    
                     <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-                    <Room scale = {0.05} position = {[0,0,0]} rotation = {[0,0,0]}/>
+                    <Room scale = {[0.1 ]} position = {[2,-10,2]} rotation = {[0,-Math.PI,0]}/>
                     <ambientLight intensity={1} />
                     <directionalLight position={[10, 10, 10]} intensity={0.5} />
                   </Suspense>
